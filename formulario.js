93ffdaf2bca1cf8010d1d088d78d5dc8b9f26a68
Felipe <felipe.dev.oliveira@gmail.com>
2019-07-19 10:08:22 -0300

    CheckBox Caputurado

--- formulario.js
+++ formulario.js
@@ -1,28 +1,50 @@
-$(document).ready(function(){
-
+function enviarDados(){
 	$('#formulario').submit(function(){
-	event.preventDefault();
+		event.preventDefault();
 
-	$.ajax({
+		$.ajax({
 
-		url: 'valida.php',
-		method: 'POST',
-		data: {
-				'nome': $('#txtNome').val(),
-				'marca': $('#txtMarca').val(),
-				'metodo': $('#metodo').val()
-				},
-		dataType: 'html'
-	}).done(function(resposta){
-		//Preenche Div Lista
-		$('#lista')	.html(resposta)
+			url: 'valida.php',
+			method: 'POST',
+			data: {
+					'nome': $('#txtNome').val(),
+					'marca': $('#txtMarca').val(),
+					'metodo': $('#metodo').val()
+					},
+			dataType: 'html'
+		}).done(function(resposta){
+			//Preenche Div Lista
+			$('#lista')	.html(resposta)
 
-		//Limpa Campos
-		$('#txtNome').val('')
-		$('#txtMarca').val('')
+			//Limpa Campos
+			$('#txtNome').val('')
+			$('#txtMarca').val('')
+		})
+	})
+}
+
+
+$(document).ready(function(){
+	
+	$('#btn_excluir').click(function(){
+		$('#metodo').val('excluir')	
+
+		if($('#check').is(':checked')){
+			alert('CHECADO')
+		}	
+		
+		enviarDados()
+	})
+
+	$('#btn_cadastrar').click(function(){
+		alert('boot')
+		$('#metodo').val('cadastrar')
+		enviarDados()
+	})
+
+
 })
-})
-})
+
 
 $(document).ready(function () {
 
--- valida.php
+++ valida.php
@@ -8,11 +8,12 @@ if (isset($_POST['status'])){
     $nome = $_POST['nome'];
     $marca = $_POST['marca'];
     $metodo = $_POST['metodo'];
-
-    if ($metodo == 'salvar') {
-
+    
+    if ($metodo == 'cadastrar') {
         $sql = mysqli_query($conn, "INSERT INTO tb_esmalte (nome_esmalte, marca_esmalte,dt_entrada) VALUES ('$nome','$marca',NOW())");
         listarDados($conn);
+    } elseif ($metodo == 'excluir'){
+        echo ('vc entrou no excluir');
     }
 }
 
@@ -21,7 +22,12 @@ function listarDados($conn){
 	$query = mysqli_query($conn, "SELECT * FROM tb_esmalte");
 
 	while ($dados = mysqli_fetch_assoc($query)) {
-		echo '<table border="1" style="border-collapse: collapse"><tr><td id = "colunanome">' .$dados['nome_esmalte'].'</td>'.'<td id="colunamarca">'.$dados['marca_esmalte'].'</td>'.'<td id="colunadata">'.$dados['dt_entrada'].'</td></tr></table>';
+        echo"<table border='1'><tr> <td><input type='checkbox' id='check'></td>
+                                    <td id=colunaid>{$dados['id_esmalte']}</td>
+                                    <td id='colunanome'>{$dados['nome_esmalte']}</td>
+                                    <td id='colunamarca'>{$dados['marca_esmalte']}</td>
+                                    <td id='colunadata'>{$dados['dt_entrada']}</td>
+                                </tr></table>";
 
 	}
 }
